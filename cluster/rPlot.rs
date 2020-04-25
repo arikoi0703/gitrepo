753f29ffa0809bf77e49eddfb0ef423e8e43a82b
arikoi0703 <arikoi0703@gmail.com>
2020-04-25 18:55:37 +0800

    update r script

--- cluster/rPlot.rs
+++ cluster/rPlot.rs
@@ -2,7 +2,7 @@ data = read.csv(file="./result/result.csv", header=FALSE, sep=",")
 x = (1:dim(data)[2])
 size = dim(data)[1]/2
 
-jpeg("iris_random_assign.jpeg")
+jpeg("random_assign.jpeg")
 color = rainbow(size)
 plot(0,0,xlab="iterator",ylab="sse",xlim=c(1,max(x)),ylim=c(min(data),max(data)),type="n")
 for ( i in 1:size ){
@@ -10,7 +10,7 @@ for ( i in 1:size ){
 }
 dev.off()
 
-jpeg("iris_with_definer.jpeg")
+jpeg("definer_assign.jpeg")
 color = rainbow(size)
 plot(0,0,xlab="iterator",ylab="sse",xlim=c(1,max(x)),ylim=c(min(data),max(data)),type="n")
 for ( i in size+1:dim(data)[1] ){
